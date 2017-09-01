78609ea6bac88bc2a262f1a388abb25831e9b5c0
Jonah Friedman <jfriedman@psyop.tv>
2017-09-01 10:19:27 -0400

    minor formatting changes in cryptomatte_utilities

--- nuke/cryptomatte_utilities.py
+++ nuke/cryptomatte_utilities.py
@@ -7,9 +7,15 @@
 
 __version__ = "1.2.0-beta5"
 
-GIZMO_CHANNEL_KNOBS = ["previewChannel", "in00", "in01", "in02", "in03", "in04", "in05", "in06", "in07"]
-GIZMO_REMOVE_CHANNEL_KNOBS = ["removePreviewChannel", "remove00", "remove01", "remove02", "remove03", "remove04", "remove05", "remove06", "remove07"]
-GIZMO_ADD_CHANNEL_KNOBS = ["addPreviewChannel", "add00", "add01", "add02", "add03", "add04", "add05", "add06", "add07"]
+GIZMO_CHANNEL_KNOBS = [
+    "previewChannel", "in00", "in01", "in02", 
+    "in03", "in04", "in05", "in06", "in07"]
+GIZMO_REMOVE_CHANNEL_KNOBS = [
+    "removePreviewChannel", "remove00", "remove01", "remove02", 
+    "remove03", "remove04", "remove05", "remove06", "remove07"]
+GIZMO_ADD_CHANNEL_KNOBS = [
+    "addPreviewChannel", "add00", "add01", "add02", "add03", 
+    "add04", "add05", "add06", "add07"]
 
 CRYPTOMATTE_METADATA_PREFIX = "exr/cryptomatte/"
 
@@ -116,7 +122,6 @@ class CryptomatteInfo(object):
         default_selection = None
         
         for key, value in exr_metadata_dict.iteritems():
-        
             if key == "input/filename":
                 self.filename = value
             if not key.startswith(CRYPTOMATTE_METADATA_PREFIX): 
@@ -713,7 +718,6 @@ def _is_number(s):
 
 
 def _set_expression(gizmo, cryptomatte_channels):
-
     matte_list_str = gizmo.knob("matteList").getValue()
     ID_list = []
 
@@ -1023,7 +1027,7 @@ def decryptomatte_nodes(nodes, ask):
         return
 
     if not ask or nuke.ask(('Replace %s Cryptomatte gizmos with expression nodes? '
-        'Replaced Gizmos will be disabled and selected.') % (len(gizmos))):
+        'Replaced Gizmos will be disabled and selected.') % len(gizmos)):
 
         for gizmo in gizmos:
             _decryptomatte(gizmo)
