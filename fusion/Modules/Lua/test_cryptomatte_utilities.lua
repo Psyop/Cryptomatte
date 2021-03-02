5131ae16fda18cca5fa22348665632d697fb3cf1
cedricduriau <duriau.cedric@live.be>
2021-03-03 00:20:53 +0100

    drop module approach; refactor test collection from globals; mock using global module; add logging tests

--- fusion/Modules/Lua/test_cryptomatte_utilities.lua
+++ fusion/Modules/Lua/test_cryptomatte_utilities.lua
@@ -10,20 +10,17 @@ Version    : 1.2.8
 local cryptoutils = require("cryptomatte_utilities")
 
 -- utils
-function collect_tests(module)
+function collect_tests()
     --[[
     Returns function names detected as test.
 
     Functions with names starting with "test_" will be picked up.
 
-    :param module: Module to collect test function names of.
-    :type module: table[string, function]
-
     :rtype: table[stri]
     ]]
     local tests = {}
-    local substr = "test_"
-    for name, _ in pairs(module) do
+    local substr = "cryptomatte_test_"
+    for name, _ in pairs(_G) do
         if string.sub(name, 1, string.len(substr)) == substr then
             table.insert(tests, name)
         end
@@ -32,7 +29,7 @@ function collect_tests(module)
     return tests
 end
 
-function run_tests(module)
+function run_tests()
     --[[
     Detects and runs all test functions of a module.
 
@@ -41,7 +38,7 @@ function run_tests(module)
     ]]
     -- collect all tests from module
     print("collectings test(s) ...")
-    local tests = collect_tests(module)
+    local tests = collect_tests()
     local ntests = #tests
     print(string.format("detected %s test(s) ...", ntests))
 
@@ -54,7 +51,7 @@ function run_tests(module)
         percentage_str = string.format("%s%s", padding, percentage_str)
         local report = string.format("[%s] %s ... ", percentage_str, name)
 
-        local status, err = pcall(module[name])
+        local status, err = pcall(_G[name])
         if status then
             report = string.format("%s [%s]", report, "OK")
         else
@@ -79,6 +76,10 @@ end
 
 -- mock funtions
 storage = {}
+
+function mock_error(message)
+    storage["error_return"] = message
+end
 
 function mock_print(message)
     storage["print_return"] = message
@@ -105,42 +106,41 @@ function mock_self_node()
 end
 
 -- tests
-module = {}
-
-function module.test__format_log()
+function cryptomatte_test__format_log()
     local old_self = self
     self = mock_self_node()
     assert_equal(cryptoutils._format_log("LEVEL", "MESSAGE"), "[Cryptomatte][NODE1][LEVEL] MESSAGE")
     self = old_self
 end
 
-function module.test__get_log_level()
-    old_get_env = os.getenv
+function cryptomatte_test__get_log_level()
+    -- store original function pre mock
+    old_get_env = _G["os"]["getenv"]
 
     -- mock log level not set in environment
-    os.getenv = mock_log_level_unset
+    _G["os"]["getenv"] = mock_log_level_unset
     local r1 = cryptoutils._get_log_level()
-    os.getenv = old_get_env
+    _G["os"]["getenv"] = old_get_env
     assert_equal(r1, 0)
 
     -- mock log level info set in environment (string -> number cast)
-    os.getenv = mock_log_level_info
+    _G["os"]["getenv"] = mock_log_level_info
     local r2 = cryptoutils._get_log_level()
-    os.getenv = old_get_env
+    _G["os"]["getenv"] = old_get_env
     assert_equal(r2, 2)
 end
 
-function module.test__string_starts_with()
+function cryptomatte_test__string_starts_with()
     assert_equal(cryptoutils._string_starts_with("foo_bar", "foo_"), true)
     assert_equal(cryptoutils._string_starts_with("foo_bar", "bar"), false)
 end
 
-function module.test__string_ends_with()
+function cryptomatte_test__string_ends_with()
     assert_equal(cryptoutils._string_ends_with("foo_bar", "_bar"), true)
     assert_equal(cryptoutils._string_ends_with("foo_bar", "foo"), false)
 end
 
-function module.test__string_split()
+function cryptomatte_test__string_split()
     result = cryptoutils._string_split("foo, bar,bunny", "([^,]+),?%s*")
     assert_equal(#result, 3)
     expected = {"foo", "bar", "bunny"}
@@ -149,7 +149,7 @@ function module.test__string_split()
     end
 end
 
-function module.test__solve_channel_name()
+function cryptomatte_test__solve_channel_name()
     -- r
     assert_equal(cryptoutils._solve_channel_name("r"), "r")
     assert_equal(cryptoutils._solve_channel_name("R"), "r")
@@ -175,17 +175,17 @@ function module.test__solve_channel_name()
     assert_equal(cryptoutils._solve_channel_name("ALPHA"), "a")
 end
 
-function module.test__get_channel_hierarchy()
+function cryptomatte_test__get_channel_hierarchy()
     -- TODO
 end
 
-function module.test__get_absolute_position()
+function cryptomatte_test__get_absolute_position()
     local x, y = cryptoutils._get_absolute_position(10, 10, 0.5, 0.5)
     assert_equal(x, 5)
     assert_equal(y, 5)
 end
 
-function module.test__is_position_in_rect()
+function cryptomatte_test__is_position_in_rect()
     -- NOTE: fusion rectangles follow mathematical convention, (origin=left,bottom)
     local rect = {left=0, top=10, right=10, bottom=0}
     assert_equal(cryptoutils._is_position_in_rect(rect, 5, 5), true)
@@ -193,37 +193,92 @@ function module.test__is_position_in_rect()
     assert_equal(cryptoutils._is_position_in_rect(rect, 5, 12), false)
 end
 
-function module.test__hex_to_float()
+function cryptomatte_test__hex_to_float()
     assert_equal(cryptoutils._hex_to_float("3f800000"), 1.0)
     assert_equal(cryptoutils._hex_to_float("bf800000"), -1.0)
 end
 
-function module.test_log_error()
+function cryptomatte_test_log_error()
+    -- store original pre mock
+    old_get_env = _G["os"]["getenv"]
+    old_self = self
+    old_print = _G["print"]
+    old_error = _G["error"]
+
+    -- mock
+    _G["os"]['getenv'] = mock_log_level_error
+    self = mock_self_node()
+    _G["print"] = mock_print
+    _G["error"] = mock_error
+
+    cryptoutils.log_error("HELP")
+
+    -- reset mock
+    _G["os"]["getenv"] = old_get_env
+    self = old_self
+    _G["print"] = old_print
+    _G["error"] = old_error
+
+    assert_equal(storage["print_return"], "[Cryptomatte][NODE1][ERROR] HELP")
+    assert_equal(storage["error_return"], "ERROR")
+end
+
+function cryptomatte_test_log_warning()
+    -- store original pre mock
+    old_get_env = _G["os"]["getenv"]
+    old_self = self
+    old_print = _G["print"]
+
+    -- mock
+    _G["os"]['getenv'] = mock_log_level_warning
+    self = mock_self_node()
+    _G["print"] = mock_print
+
+    cryptoutils.log_warning("HELP")
+
+    -- reset mock
+    _G["os"]["getenv"] = old_get_env
+    self = old_self
+    _G["print"] = old_print
+
+    assert_equal(storage["print_return"], "[Cryptomatte][NODE1][WARNING] HELP")
+end
+
+function cryptomatte_test_log_info()
+    -- store original pre mock
+    old_get_env = _G["os"]["getenv"]
+    old_self = self
+    old_print = _G["print"]
+
+    -- mock
+    _G["os"]['getenv'] = mock_log_level_info
+    self = mock_self_node()
+    _G["print"] = mock_print
+
+    cryptoutils.log_info("HELP")
+
+    -- reset mock
+    _G["os"]["getenv"] = old_get_env
+    self = old_self
+    _G["print"] = old_print
+
+    assert_equal(storage["print_return"], "[Cryptomatte][NODE1][INFO] HELP")
+end
+
+function cryptomatte_test_get_cryptomatte_metadata()
     -- TODO
 end
 
-function module.test_log_warning()
+function cryptomatte_test_read_manifest_file()
     -- TODO
 end
 
-function module.test_log_info()
+function cryptomatte_test_decode_manifest()
     -- TODO
 end
 
-function module.test_get_cryptomatte_metadata()
+function cryptomatte_test_get_matte_names()
     -- TODO
 end
 
-function module.test_read_manifest_file()
-    -- TODO
-end
-
-function module.test_decode_manifest()
-    -- TODO
-end
-
-function module.test_get_matte_names()
-    -- TODO
-end
-
-run_tests(module)
+run_tests()
